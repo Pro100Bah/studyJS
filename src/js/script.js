fb9dcbf5cccab5a5f637c4f05d28cdad21299fb9
Pro100Bah <seregasalof1991@yandex.ru>
2020-04-02 19:14:33 +0500

    Lesson 3, HW 3.1

--- src/js/script.js
+++ src/js/script.js
@@ -19,44 +19,50 @@ let mainList = {
   shopGoods: [],
   employers: {},
   open: false,
-  discount: false
-};
+  discount: false,
+  chooseGoods: function chooseGoods() {
+    for (i = 0; i < 5; i++) {
+      let question = prompt("Какой тип товаров будем продавать?", "");
 
-function chooseGoods() {
-  for (i = 0; i < 5; i++) {
-    let question = prompt("Какой тип товаров будем продавать?", "");
-
-    if (
-      typeof question === "string" &&
-      typeof question != null &&
-      question != "" &&
-      question.length < 50
-    ) {
-      console.log("Все верно!!!");
-      mainList.shopGoods[i] = question;
+      if (
+        typeof question === "string" &&
+        typeof question != null &&
+        question != "" &&
+        question.length < 50
+      ) {
+        console.log("Все верно!!!");
+        mainList.shopGoods[i] = question;
+      } else {
+        i = i - 1; //HW 2
+      }
+    }
+  },
+  workTime: function workTime(time) {
+    if (time < 0) {
+      console.log("Такого просто не может быть!!!");
+    } else if (time > 8 && time < 20) {
+      console.log("Время работать!!!");
+      mainList.open = true;
+    } else if (time < 24) {
+      console.log("Уже слишком поздно!!!");
     } else {
-      i = i - 1; //HW 2
+      console.log("В сутках только 24 часа!!!");
+    }
+  },
+  dayBudget: function dayBudget() {
+    alert("Ежедневный бюджет: " + mainList.budget / 30);
+  },
+  makeDiscount: function makeDiscount() {
+    if (mainList.discount == true) {
+      price = (price / 100) * 80;
+    }
+  },
+  hireEmployers: function hireEmployers() {
+    for (let i = 1; i < 4; i++) {
+      let name = prompt("Имя сотрудника?", "");
+      mainList.employers[i] = name;
     }
   }
-}
-
-// chooseGoods();
-
-function workTime(time) {
-  if (time < 0) {
-    console.log("Такого просто не может быть!!!");
-  } else if (time > 8 && time < 20) {
-    console.log("Время работать!!!");
-  } else if (time < 24) {
-    console.log("Уже слишком поздно!!!");
-  } else {
-    console.log("В сутках только 24 часа!!!");
-  }
-}
-// workTime(time);
-
-function dayBudget() {
-  alert("Ежедневный бюджет: " + mainList.budget / 30);
-}
+};
 
 console.log(mainList);
